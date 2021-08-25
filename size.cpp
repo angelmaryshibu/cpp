de6bcebfbdb36ccef8027337ed6201dc6bffb8d0
angelmaryshibu <86075805+angelmaryshibu@users.noreply.github.com>
2021-08-25 22:39:45 +0530

    Create size.cpp

---
+++ size.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using std::cout;
+
+int main()
+{
+   cout <<"size of char=1 byte \n size of int=4 bytes \n size of float=4 bytes \n size of double=8 bytes";
+   
+   return 0;
+}
