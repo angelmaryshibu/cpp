7ff1ae17a5fb1b5410c6e39bc6e55fce28ec2af4
angelmaryshibu <86075805+angelmaryshibu@users.noreply.github.com>
2021-08-27 19:33:28 +0530

    Create vote.cpp

---
+++ vote.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int age;
+  cout<<"enter the age of the user";
+  cin>>age;
+  if(age>=18)
+  {cout<<"you are eligible for voting";
+  }
+  else
+  cout<<"you are not eligible for voting";
+    return 0;
+}
